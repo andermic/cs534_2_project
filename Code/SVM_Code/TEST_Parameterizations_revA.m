24cec334c38dbf798dfb4c2dd61a4a401e5c6c65
Gregory Gutshall <gregory.gutshall@gmail.com>
2012-05-19 14:46:42 -0700

    Created a SVM_Code Directory for me to play around in

---
+++ Code/SVM_Code/TEST_Parameterizations_revA.m
@@ -0,0 +1,10 @@
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Create Parameterized Vectors for SVM
+% Gregory Gutshall
+% Date: 05/19/2012
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% This code is used to create parameter vectors for use with the
+% subgradient Pegasos SVM code.
+
+% Load in the orginal MATLAB dataset
+load('../Orginal_Dataset/base_data.mat');
