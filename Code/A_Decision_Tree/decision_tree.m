985ac8a0680ad6356428a80878315dfca04966b9
Michael Anderson <andermic@kec1130-35.engr.oregonstate.edu>
2012-05-20 20:48:26 -0700

    optimized meta_featurize just for kicks and giggles, ran experiment w/ checkmate feature, in the bug fixing stage with decision tree

--- Code/A_Decision_Tree/decision_tree.m
+++ Code/A_Decision_Tree/decision_tree.m
@@ -1,22 +1,54 @@
-function [tree] = decision_tree(features, classes)
+function [tree] = decision_tree(features, classes, feature_val)
 % Return a tree that represents splitting of the training data so that
 % leaves of the data contain only elements from one class.
 
+PARENT = 1;
+FEATURE_VAL = 2;
+CLASS_VAL = 3;
+
+node = zeros(3,1);
+node(PARENT) = 0;
+node(FEATURE_VAL) = feature_val;
+node(CLASS_VAL) = -2;
+
 % Base case: Return if all of the data belong to the same class.
-if (size(classes,1) == 1) || (sum(classes(1) ~= classes) == 0)
-    return
-end
+if sum(classes(1) ~= classes) == 0
+    node(CLASS_VAL) = classes(1);
+    tree = node;
 
-% Find the feature with the highest information gain.
-mut_infs = arrayfun(@(i) mutual_information(features(:,i),classes), 1:size(features,2));
-best = find(max(mut_infs) == mut_infs);
-if size(best,2) > 1
-    best = best(1);
-end
+% Recursive case: Perform a split.
+else
+    % Find the feature with the highest mutual information gain.
+    mut_infs = arrayfun(@(i) mutual_information(features(:,i),classes), 1:size(features,2));
+    best = find(max(mut_infs) == mut_infs);
+    if size(best,2) > 1
+        best = best(1);
+    end
 
-for i = unique(features(best))
-    
+    % Split using the given feature.
+    tree = node;
+    for i = unique(features(:,best))'
+        feat_places = features(:,best) == i;
+        new_feats = features(feat_places);
+        new_classes = classes(feat_places);
+        child = decision_tree(new_feats, new_classes, i);
+        child(1,:) = child(1,:) + size(tree,2);
+        tree = [tree, child];
+    end
 end
+clear PARENT;
+clear FEATURE_VAL;
+clear CLASS_VAL;
+clear features;
+clear classes;
+clear feature_val;
+clear node;
+clear mut_infs;
+clear best;
+clear feat_places;
+clear i
+clear new_feats;
+clear new_classes;
+clear child;
 
 end
-
--- Code/A_Decision_Tree/meta_featurize.m
+++ Code/A_Decision_Tree/meta_featurize.m
@@ -1,5 +1,5 @@
 function [new_data] = meta_featurize(base_data, norm)
-% Reparameterize the base data in terms of 3 meta-features which are
+% Reparameterize the base data in terms of 4 meta-features which are
 % described below.
 
 % Feature 1: Distance of the black king to the edge of the board.
@@ -16,12 +16,12 @@ function [new_data] = meta_featurize(base_data, norm)
 % position is checkmate iff the two kings are correctly opposed, the black
 % king is on the edge, and the rook is on the same edge as the black king.
 
-% Outputs an N by 4 matrix, where N is the number of chess positions in the
-% dataset. The first 3 columns of the matrix are the 3 meta-features for
-% each position, the 4th column is the class label for each position.
+% Outputs an N by 5 matrix, where N is the number of chess positions in the
+% dataset. The first 4 columns of the matrix are the 4 meta-features for
+% each position, the 5th column is the class label for each position.
 
 
-% These constants will be useful
+% These constants will be useful.
 WKC = base_data(:,1);
 WKR = base_data(:,2);
 WRC = base_data(:,3);
@@ -48,86 +48,61 @@ end
 
 
 % Feature 3
-stalemate = zeros(size(base_data,1),1);
-free_rook = zeros(size(base_data,1),1);
-for i = 1:size(base_data,1)
-    % Check for stalemate.
-    % Stalemate can only occur if the black king is in the corner.
-    if (col_dists(i) == 0) && (row_dists(i) == 0)
-        % 1st stalemate check. The rook is one column and one row away from
-        % the black king.
-        if (abs(BKC(i) - WRC(i)) == 1) && (abs(BKR(i) - WRR(i)) == 1)
-            % Also, the white king protects the rook
-            if (max(abs(WKC(i) - WRC(i)), abs(WKR(i) - WRR(i)) == 1))
-                stalemate(i) = 1;
-            end
-        % 2nd stalemate check. The rook is one row away from the black king
-        %  and the white king is two rows away on the same column.
-        elseif (abs(BKC(i) - WRC(i)) == 1) && (abs(BKR(i) - WKR(i)) == 2) && (BKC(i) == WKC(i))
-            stalemate(i) = 1;
-        % Must check reverse of row/column logic in previous.
-        elseif (abs(BKR(i) - WRR(i)) == 1) && (abs(BKC(i) - WKC(i)) == 2) && (BKR(i) == WKR(i))
-            stalemate(i) = 1;
-        end
-    end
-    
-    % Check for capturable rook.
-    % Black king must be next to the rook, white king must not be next to
-    % the rook.
-    if (max(abs(BKC(i)-WRC(i)),abs(BKR(i)-WRR(i))) == 1) && (max(abs(WKC(i)-WRC(i)),abs(WKR(i)-WRR(i))) > 1)
-        free_rook(i) = 1;
-    end
-end
+
+% Check for stalemate.
+% Stalemate can only occur if the black king is in the corner.
+eval_strs = ['(col_dists == 0) & (row_dists == 0)'];
+% 1st stalemate check. The rook is one column and one row away from
+% the black king.
+eval_strs = [eval_strs, '& (((abs(BKC - WRC) == 1) & (abs(BKR - WRR) == 1)'];
+% Also, the white king protects the rook
+eval_strs = [eval_strs, '& max(abs(WKC - WRC), abs(WKR - WRR) == 1))'];
+% 2nd stalemate check. The rook is one row away from the black king and
+% the white king is two rows away on the same column.
+eval_strs = [eval_strs, '| ((abs(BKC - WRC) == 1) & (abs(BKR - WKR) == 2) & (BKC == WKC))'];
+% Must check reverse of row/column logic in previous.
+eval_strs = [eval_strs, '| ((abs(BKR - WRR) == 1) & (abs(BKC - WKC) == 2) & (BKR == WKR)))'];
+stalemate = eval(strcat(eval_strs));
+
+% Check for capturable rook.
+% Black king must be next to the rook, white king must not be next to
+% the rook.
+free_rook = (max(abs(BKC-WRC),abs(BKR-WRR)) == 1) & (max(abs(WKC-WRC),abs(WKR-WRR)) > 1);
+
 feature_3 = stalemate | free_rook;
 
 
 % Feature 4
-feature_4 = zeros(size(base_data,1),1);
-for i = 1:size(base_data,1)
-    % Black king must be on an edge, rook must be on the same edge but not
-    % right next to the black king.
-    if (BKC(i) == 1) && (WRC(i) == 1) && (abs(BKR(i) - WRR(i)) > 1)
-        % White king must be 2 squares away.
-        if WKC(i) == 3
-            % White king must be directly opposed.
-            if WKR(i) == BKR(i)
-                feature_4(i) = 1;
-            % Also if the black king is in the corner, the kings do not
-            % need to be perfectly opposed.
-            elseif ((BKR(i) == 1) || (BKR(i) == 8)) && (abs(BKR(i) - WKR(i)) == 1)
-                feature_4(i) = 1;
-            end
-        end
-    end
-    % Duplicate this code for each of the 4 edges on the chessboard
-    if (BKC(i) == 8) && (WRC(i) == 8) && (abs(BKR(i) - WRR(i)) > 1)
-        if WKC(i) == 6
-            if WKR(i) == BKR(i)
-                feature_4(i) = 1;
-            elseif ((BKR(i) == 1) || (BKR(i) == 8)) && (abs(BKR(i) - WKR(i)) == 1)
-                feature_4(i) = 1;
-            end
-        end
-    end
-    if (BKR(i) == 1) && (WRR(i) == 1) && (abs(BKC(i) - WRC(i)) > 1)
-        if WKR(i) == 3
-            if WKC(i) == BKC(i)
-                feature_4(i) = 1;
-            elseif ((BKC(i) == 1) || (BKC(i) == 8)) && (abs(BKC(i) - WKC(i)) == 1)
-                feature_4(i) = 1;
-            end
-        end
-    end
-    if (BKR(i) == 8) && (WRR(i) == 8) && (abs(BKC(i) - WRC(i)) > 1)
-        if WKC(i) == 6
-            if WKC(i) == BKC(i)
-                feature_4(i) = 1;
-            elseif ((BKC(i) == 1) || (BKC(i) == 8)) && (abs(BKC(i) - WKC(i)) == 1)
-                feature_4(i) = 1;
-            end
-        end
-    end
 
-    
+% Black king must be on an edge, rook must be on the same edge but not
+% right next to the black king.
+eval_strs = '(BKC == 1 & WRC == 1 & (abs(BKR - WRR) > 1)';
+% White king must be 2 squares away.
+eval_strs = [eval_strs, '& WKC == 3'];
+% White king must be directly opposed.
+eval_strs = [eval_strs, '& (WKR == BKR'];
+% Or if the black king is the in the corner, the kings do not need to be
+% perfectly opposed.
+eval_strs = [eval_strs, '| ((BKR == 1 | BKR == 8) & (abs(BKR - WKR) == 1))))'];
+
+% Run analogous tests for each of the 4 edges of a chessboard.
+eval_strs = [eval_strs, '| (BKC == 8 & WRC == 8 & (abs(BKR - WRR) > 1)'];
+eval_strs = [eval_strs, '& WKC == 6'];
+eval_strs = [eval_strs, '& (WKR == BKR'];
+eval_strs = [eval_strs, '| ((BKR == 1 | BKR == 8) & (abs(BKR - WKR) == 1))))'];
+
+eval_strs = [eval_strs, '| (BKR == 1 & WRR == 1 & (abs(BKC - WRC) > 1)'];
+eval_strs = [eval_strs, '& WKR == 3'];
+eval_strs = [eval_strs, '& (WKC == BKC'];
+eval_strs = [eval_strs, '| ((BKC == 1 | BKC == 8) & (abs(BKC - WKC) == 1))))'];
+
+eval_strs = [eval_strs, '| (BKR == 8 & WRR == 8 & (abs(BKC - WRC) > 1)'];
+eval_strs = [eval_strs, '& WKR == 6'];
+eval_strs = [eval_strs, '& (WKC == BKC'];
+eval_strs = [eval_strs, '| ((BKC == 1 | BKC == 8) & (abs(BKC - WKC) == 1))))'];
+
+feature_4 = eval(eval_strs);
+
+
 new_data = [feature_1,feature_2,feature_3,feature_4,CLASS];
 end
