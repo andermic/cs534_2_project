a249b205be3fdf5047e7b33138c4151edba99efd
Gregory Gutshall <gregory.gutshall@gmail.com>
2012-05-21 17:35:17 -0700

    Added a test for the two class [1,-1] SVM

---
+++ Code/SVM_Code/mainscript_revA.m
@@ -0,0 +1,14 @@
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% mainscript
+% Gregory Gutshall
+% Date: 05/21/2012
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% This script is used to call other functions and plotting scripts
+
+% Load in the test data
+load('Test_Parameterizations.mat');
+
+% Create a Kernel Function
+
+
+
